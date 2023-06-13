67fcf5838ff655736c144e7bad2673cfb95956a7
Niwaiwa <po2846@gmail.com>
2023-06-13 23:12:29 +0800

    feat: add body block and change footer bk color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Typography, Box } from '@mui/material';
 
 export default function Footer() {
   return (
-    <Box mt={5} py={3} textAlign="center" bgcolor="grey.100">
+    <Box mt={5} py={3} textAlign="center">
       <Typography variant="body2" color="text.secondary">
         © 2023 Niwa graden
       </Typography>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Container } from '@mui/material';
+import { Container, Box, Typography, Button, Grid, CardMedia } from '@mui/material';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 
@@ -8,7 +8,36 @@ export default function HomePage() {
   return (
     <Container>
       <Navigation />
-      {/* Your body content here */}
+      <Box  display="flex" flexDirection="column" justifyContent="center" alignItems="center" minHeight="60vh">
+        <Typography variant="h3" gutterBottom>
+          My site and portfolio
+        </Typography>
+        <Button variant="contained" color="primary">
+          Browse my portfolio
+        </Button>
+      </Box>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs={12} md={5}>
+          <Box minHeight={400}>
+            <CardMedia
+                component="img"
+                height="693"
+                image="/images/tech_light.jpg"
+                alt="Vertical Image"
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={7}>
+          <Box minHeight={200}>
+            <CardMedia
+                component="img"
+                height="520"
+                image="/images/tech_work.jpg"
+                alt="Horizontal Image"
+            />
+          </Box>
+        </Grid>
+      </Grid>
       <Footer />
     </Container>
   );
