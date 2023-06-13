56680aaadc7bb41b64a09917ba3f78390a5b54bf
Niwaiwa <po2846@gmail.com>
2023-06-13 23:35:28 +0800

    feat: add bk color white and change navi color

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,15 +2,15 @@ import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 export default function Navigation() {
   return (
-    <AppBar position="static">
+    <AppBar position="static" color="transparent" elevation={0}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Niwa Garden
         </Typography>
-        <Button color="inherit">Profile</Button>
-        <Button color="inherit">Portfolio</Button>
-        <Button color="inherit">Tech note</Button>
-        <Button color="inherit">Github</Button>
+        <Button color="inherit" sx={{ marginRight: 2 }}>Profile</Button>
+        <Button color="inherit" sx={{ marginRight: 2 }}>Portfolio</Button>
+        <Button color="inherit" sx={{ marginRight: 2 }}>Tech note</Button>
+        <Button color="primary" variant="contained">Github</Button>
         {/* Add more navigation buttons as needed */}
       </Toolbar>
     </AppBar>
