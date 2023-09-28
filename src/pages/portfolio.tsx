11b603b2c4a16d1fa669689ea67886a53d968fbb
Niwaiwa <po2846@gmail.com>
2023-09-28 21:52:19 +0800

    feat: add portfolio tag filter

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,41 +1,54 @@
-import { Card, CardMedia, CardContent, Typography, CardActions, Button, Grid, Box, Container, Avatar } from '@mui/material';
-import Navigation from '../components/Navigation';
-import Footer from '../components/Footer';
+import { Card, CardMedia, CardContent, Typography, CardActions, Button, Grid, Box, Container } from '@mui/material';
 import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
-import portfolioData from '../portforlioData.json';
+import portfolioData from '../portfolioData.json';
+import React, { useState } from 'react';
 
 
 function Portfolio() {
+
+  const [filterPortfolioData, setFilterPortfolioData] = useState(portfolioData)
+  const tagList = ["Python", "Flask", "Django", "FastAPI", "Backend", "Frontend", "Typescript", "React", "Next.js", "Go", "Echo", "Gin", "Streamlit"]
+  const [selectTags, setSelectTags] = useState<string[]>([])
+
+  const tagFilter = (tag: string) => {
+    if (selectTags.includes(tag)) {
+      const newTags = selectTags.filter(item => item !== tag)
+      setSelectTags(newTags)
+      if (newTags.length === 0) {
+        setFilterPortfolioData(portfolioData)
+      } else {
+        const newFilterData = portfolioData.filter(item => {
+          return item.tags.some(tag => newTags.includes(tag))
+        })
+        setFilterPortfolioData(newFilterData)
+      }
+    } else {
+      const newTags = [...selectTags, tag]
+      setSelectTags(newTags)
+      const newFilterData = portfolioData.filter(item => {
+        return item.tags.some(tag => newTags.includes(tag))
+      })
+      setFilterPortfolioData(newFilterData)
+    }
+  }
+
   return (
     <Container>
         <Grid container spacing={3} sx={{ marginTop: 5 }}>
-        {portfolioData.map((project, index) => {
-            return (
-            <Grid item xs={12} sm={6} md={4} key={index}>
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        sx={{ height: 140 }}
-                        image={project.image}
-                        title={project.title}
-                    />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {project.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {project.description}
-                    </Typography>
-                    <Box sx={{ marginTop: 2 }}>
-                      {project.tags.map((tag, index) => {
-                          return (
+          <Grid item xs={12} sm={12} md={12} sx={{ marginBottom: 3 }}>
+              <Typography variant="h3" gutterBottom>
+                  Portfolio
+              </Typography>
+              <Box sx={{ marginTop: 2 }} >
+                  {tagList.map((tag, index) => {
+                      return (
                           <Chip
                               key={index}
                               label={tag}
                               size="small"
                               color="primary"
-                              variant="outlined"
+                              variant={selectTags.includes(tag) ? "filled" : "outlined"}
                               sx={{ 
                                 marginRight: '5px', 
                                 marginTop: '5px',
@@ -43,26 +56,64 @@ function Portfolio() {
                                 borderRadius: '5px',
                                 padding: '2px',
                               }}
-                              
+                              onClick={() => tagFilter(tag)}
                           />
-                          );
-                      })}
-                    </Box>
-                    </CardContent>
-                    <Divider />
-                    <CardActions>
-                    <Box sx={{ marginTop: 1 }} />
-                    <Button size="small" color="primary" href={project.browseUrl} {...(project.browseUrl === "" ? { disabled: true } : {}) }>
-                        Browse
-                    </Button>
-                    <Button size="small" color="primary" href={project.githubUrl}>
-                        GitHub
-                    </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            );
-        })}
+                      );
+                  })}
+              </Box>
+          </Grid>
+          {filterPortfolioData.map((project, index) => {
+              return (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                  <Card sx={{ maxWidth: 345 }}>
+                      <CardMedia
+                          sx={{ height: 140 }}
+                          image={project.image}
+                          title={project.title}
+                      />
+                      <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                          {project.title}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                          {project.description}
+                      </Typography>
+                      <Box sx={{ marginTop: 2 }}>
+                        {project.tags.map((tag, index) => {
+                            return (
+                            <Chip
+                                key={index}
+                                label={tag}
+                                size="small"
+                                color="primary"
+                                variant="outlined"
+                                sx={{ 
+                                  marginRight: '5px', 
+                                  marginTop: '5px',
+                                  height: 'auto',
+                                  borderRadius: '5px',
+                                  padding: '2px',
+                                }}
+                                
+                            />
+                            );
+                        })}
+                      </Box>
+                      </CardContent>
+                      <Divider />
+                      <CardActions>
+                      <Box sx={{ marginTop: 1 }} />
+                      <Button size="small" color="primary" href={project.browseUrl} {...(project.browseUrl === "" ? { disabled: true } : {}) }>
+                          Browse
+                      </Button>
+                      <Button size="small" color="primary" href={project.githubUrl}>
+                          GitHub
+                      </Button>
+                      </CardActions>
+                  </Card>
+              </Grid>
+              );
+          })}
         </Grid>
     </Container>
   );
